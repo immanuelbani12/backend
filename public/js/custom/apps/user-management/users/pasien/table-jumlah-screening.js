74d0be97c7a3ef48163c3c08596a2339724a3ed5
HexaTeam6 <wahed.18051@mhs.its.ac.id>
2022-05-16 18:42:15 +0700

    edit pesan wa blast

--- public/js/custom/apps/user-management/users/pasien/table-jumlah-screening.js
+++ public/js/custom/apps/user-management/users/pasien/table-jumlah-screening.js
@@ -21,16 +21,17 @@ var KTUsersList = function () {
                     text: 'Ingatkan pasien',
                     action: function () {
                         var data = datatable.rows( { selected: true } ).data();
+                        var text = "Sudah waktunya untuk screening risiko penyakit anda, pastikan anda terbebas dari risiko penyakit Diabetes, Kolesterol dan Stroke! Silahkan install aplikasi apadok di link berikut https://play.google.com/store/apps/details?id=com.apadok.emrpreventive"
 
                         for (let index = 0; index < data.length; ++index) {
-                            const nomor = data[index][1];
+                            const nomor = "6281227676935"
                             
                             $.ajax({
                                 url : "https://api.chat-api.com/instance216488/sendMessage?token=cls7zrc15wq7o1sm",
                                 type: "POST",
                                 data: JSON.stringify({
                                     phone: nomor,
-                                    body: "WhatsApp API on Chat API works good"
+                                    body: text
                                 }),
                                 contentType: "application/json; charset=utf-8",
                                 dataType   : "json",
